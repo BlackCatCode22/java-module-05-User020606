3d96f11550093e1cf776efbcb391e2bc59ed5a1e
User020606 <vincentmoua06@gmail.com>
2025-10-14 11:45:32 -0700

    Add files via upload

---
+++ Tigers.java
@@ -0,0 +1,20 @@
+public class Tigers extends Animal {
+
+
+    private static int numOfTigers = 0;
+
+    public Tigers() {
+        numOfTigers++;
+    }
+
+    public static int getNumOfTigers() {
+        return numOfTigers;
+    }
+
+
+
+
+
+}
+
+
