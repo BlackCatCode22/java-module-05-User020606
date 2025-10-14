c9115acd9396e620ea7e2f6fed82735f7826c78c
User020606 <vincentmoua06@gmail.com>
2025-10-14 11:45:47 -0700

    Add files via upload

---
+++ Hyenas.java
@@ -0,0 +1,20 @@
+public class Hyenas extends Animal {
+
+
+    private static int numOfHyenas = 0;
+
+    public Hyenas() {
+        numOfHyenas++;
+    }
+
+    public static int getNumOfHyenas() {
+        return numOfHyenas;
+    }
+
+
+
+
+
+}
+
+
