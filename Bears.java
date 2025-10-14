4a3e0ff9f3c0859de0c6e2000a919b636d22044a
User020606 <vincentmoua06@gmail.com>
2025-10-14 11:45:06 -0700

    Add files via upload

---
+++ Bears.java
@@ -0,0 +1,20 @@
+public class Bears extends Animal {
+
+
+    private static int numOfBears = 0;
+
+    public Bears() {
+        numOfBears++;
+    }
+
+    public static int getNumOfBears() {
+        return numOfBears;
+    }
+
+
+
+
+
+}
+
+
